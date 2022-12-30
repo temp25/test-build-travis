2b213cd60cca293c48706faa0396582676cd4b31
temp25 <39752837+temp25@users.noreply.github.com>
2022-12-30 19:43:56 +0530

    Update build_aria2.sh

--- build_aria2.sh
+++ build_aria2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt install -y build-essential curl wget make git vim bash autoconf autopoint libtool pkg-config
+apt install -y build-essential curl wget make git vim bash autoconf autopoint libtool pkg-config docutils
 cd aria2-static-builds/build-scripts/gnu-linux-config
 git clone https://github.com/aria2/aria2.git
 ./aria2-x86_64-gnu-linux-build-libs
@@ -9,3 +9,6 @@ cd aria2
 autoreconf -i
 ../aria2-x86_64-gnu-linux-config
 make
+strip --strip-all src/aria2c
+cp src/aria2c $HOME/final_build/
+ls -lahtr $HOME/final_build
