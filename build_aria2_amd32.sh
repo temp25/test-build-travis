7cad3ce3d1093faab39164e5ce84b22b88b03645
temp25 <39752837+temp25@users.noreply.github.com>
2023-01-01 21:28:49 +0530

    Added shell scripts to build for Mingw 32 and 64 bit architectures

--- build_aria2_amd32.sh
+++ build_aria2_amd32.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 CWD="$(pwd)"
-CI_HOME="/home/circleci"
+TEMP_DIR="/tmp"
 
-cd "${CI_HOME}"
 apt-get update
-apt install -y build-essential curl wget make git vim bash autoconf autopoint libtool pkg-config libcppunit-dev zlib1g-dev sqlite3 libsqlite3-dev python3-docutils
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends ca-certificates build-essential curl wget make git vim bash autoconf autopoint libtool pkg-config libcppunit-dev zlib1g-dev sqlite3 libsqlite3-dev python3-docutils
 git clone https://github.com/q3aql/aria2-static-builds.git
 cd aria2-static-builds/build-scripts/gnu-linux-config
 git clone https://github.com/aria2/aria2.git
@@ -15,10 +14,9 @@ autoreconf -i
 ../aria2-i386-gnu-linux-cross-config
 make
 strip --strip-all src/aria2c
-mkdir -p "${CI_HOME}/final_build"
-echo "Storing artifact 'aria2c_amd32' to ${CI_HOME}/final_build"
-cp src/aria2c "${CI_HOME}/final_build/aria2c_amd32"
-ls -lahtr "${CI_HOME}/final_build"
+mkdir -p "${TEMP_DIR}/final_build"
+echo "Storing artifact 'aria2c_amd32' to ${TEMP_DIR}/final_build"
+cp src/aria2c "${TEMP_DIR}/final_build/aria2c_amd32"
+ls -lahtr "${TEMP_DIR}/final_build"
 
 cd "${CWD}"
-
--- build_aria2_amd64.sh
+++ build_aria2_amd64.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 CWD="$(pwd)"
-CI_HOME="/home/circleci"
+TEMP_DIR="/tmp"
 
-cd "${CI_HOME}"
 apt-get update
-apt install -y build-essential curl wget make git vim bash autoconf autopoint libtool pkg-config docutils
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends ca-certificates build-essential curl wget make git vim bash autoconf autopoint libtool pkg-config docutils
 git clone https://github.com/q3aql/aria2-static-builds.git
 cd aria2-static-builds/build-scripts/gnu-linux-config
 git clone https://github.com/aria2/aria2.git
@@ -15,9 +14,9 @@ autoreconf -i
 ../aria2-x86_64-gnu-linux-config
 make
 strip --strip-all src/aria2c
-mkdir -p "${CI_HOME}/final_build"
-cp src/aria2c "${CI_HOME}/final_build/aria2c_amd64"
-ls -lahtr "${CI_HOME}/final_build"
+mkdir -p "${TEMP_DIR}/final_build"
+echo "Storing artifact 'aria2c_amd64' to ${TEMP_DIR}/final_build"
+cp src/aria2c "${TEMP_DIR}/final_build/aria2c_amd64"
+ls -lahtr "${TEMP_DIR}/final_build"
 
 cd "${CWD}"
-
